b10464271b97e172baac7e67dc7767c5d16d030a
Nimit Johri <nimit.johri@nagarro.com>
2020-04-28 15:18:14 +0530

    remove extra project

--- Bestbuy/services/aggregator/src/main/java/org/bestbuy/aggregator/service/AggregatorDetailService.java
+++
@@ -1,8 +0,0 @@
-package org.bestbuy.aggregator.service;
-
-import org.bestbuy.aggregator.mdels.OrderDetail;
-
-public interface AggregatorDetailService {
-
-	public OrderDetail getUserOrderDetails(Integer userId);
-}
